f8e13d9ba5d23439438b3e975b1c3f2cc0495d5c
JUB_Yoush <yoush1337@gmail.com>
2023-11-23 19:49:16 -0500

    up

---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+import adapter from '@sveltejs/adapter-static';
+import sveltePreprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex'
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			pages:'build',
+			assets:'build',
+			fallback:undefined,
+			precompress: false,
+			strict: true
+	})
+	},
+	extensions:[
+		'.svelte',
+		'.svx',
+		'.md'
+	],
+	 preprocess: [
+    sveltePreprocess(),
+    mdsvex({
+      extensions: ['.md']
+    })
+  ]
+};
+
+export default config;
